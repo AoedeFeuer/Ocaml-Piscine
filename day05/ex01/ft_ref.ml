94e632cf87cc0c65f92d97d391c85abbcd47bdb6
Qweissna <>
2021-08-03 11:46:42 +0300

    add

---
+++ day05/ex01/ft_ref.ml
@@ -0,0 +1,8 @@
+module type ft_ref = 
+sig
+  type 'a ft_ref
+  val return : 'a -> 'a ft_ref
+  val get : 'a ft_ref -> 'a
+  val set : 'a ft_ref -> 'a -> unit
+  val bind : 'a ft_ref -> ('a -> 'b ft_ref) -> 'b ft_ref
+end
