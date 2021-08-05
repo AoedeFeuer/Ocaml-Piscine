5f35c32c2f178a8c1041d33d0747724bd71e3719
Qarl Weissnat <qweissna@il-g3.msk.21-school.ru>
2021-08-05 14:03:26 +0300

    add d3

--- day03/ex00/ft_graphics.ml
+++ day03/ex00/ft_graphics.ml
@@ -7,31 +7,35 @@ let draw_square x y z =
   Graphics.lineto ((x - z / 2) + z) (y + z / 2);
   Graphics.moveto (x - z / 2) (y - z / 2);
   Graphics.lineto ((x - z / 2) + z) (y - z / 2);
-  Graphics.moveto (x + z / 2) (y + z / 2);
-  Graphics.lineto (x + z / 2) ((y + z / 2) + z);
+  Graphics.moveto (x - z / 2) (y - z / 2);
+  Graphics.lineto (x - z / 2) ((y - z / 2) + z);
   Graphics.moveto (x + z / 2) (y - z / 2);
   Graphics.lineto (x + z / 2) ((y - z / 2) + z)
 
-let draw_tree_node node = function
-  | [] -> []
-  | h :: t -> 
+let draw_tree_node Node  = 
+(**  | [] -> []
+  | h :: t -> *)
   draw_square 300 300 100;
   draw_square 600 400 100;
   draw_square 600 500 100;
   Graphics.moveto (300 + 100 / 2) (300 + 100 / 2);
-  Graphics.lineto (600 - 100 / 2) (400 - z / 2);
+  Graphics.lineto (600 - 100 / 2) (400 - 100 / 2);
   Graphics.moveto (300 + 100 / 2) (300 + 100 / 2);
-  Graphics.lineto (500 - 100 / 2) (500 - z / 2);
+  Graphics.lineto (500 - 100 / 2) (500 - 100 / 2);
   Graphics.moveto 300 300;
-  Graphics.draw_string(h);
+  Graphics.draw_string p;
   Graphics.moveto 600 400;
-  Graphics.draw_string("Nil");
+  Graphics.draw_string c1;
   Graphics.moveto 600 500;
-  Graphics.draw_string("Nil")
+  Graphics.draw_string c2
 
 
 let main () =
-  Graphics.open_graph "800x600";
-  draw_square(300 400 20)
+  Graphics.open_graph " 800x600";
+  draw_tree_node Node ("42", Nil, Nil);
+(*  draw_square 300 400 100;
+  Graphics.moveto 400 300;
+  Graphics.draw_string "42";*)
+  Graphics.read_key ()
 
 let _ = main()
