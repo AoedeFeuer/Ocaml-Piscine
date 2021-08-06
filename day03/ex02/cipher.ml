bbb6f2452393701b5cfa74e64bf7c68faae6573e
Qweissna <>
2021-08-06 11:21:27 +0300

    backup

--- day03/ex02/cipher.ml
+++ day03/ex02/cipher.ml
@@ -27,16 +27,31 @@ let caesar x rot_str =
   in String.map shift_fun rot_str
 
 let xor key src_str =
-  String.map (function x -> char_of_int ((int_of_char x )lxor key)) src_str
+  let shift_fun c =
+    if up_case c || low_case c
+    then char_of_int (int_of_char c lxor (key mod 26))
+    else c
+  in String.map shift_fun src_str
 
 let rec ft_crypt (src_str: string) func = 
   match func with
   | h :: t -> ft_crypt (h src_str) t
-  | [] -> "End"
+  | [] -> src_str
 
 let main () =
-  print_endline(rot42 "abcde");
-  print_endline(caesar 1 "abcde");
-  print_endline(xor 1 "abcde")
+  print_endline("CASE 1");
+  print_endline(rot42 "abcde1");
+  print_endline(caesar 1 "abcde1");
+  print_endline(xor 1 "abcde1");
+  print_endline(rot42 (caesar 1 (xor 1 "abcde")));
+  let func_list = [xor 1;caesar 1;rot42] in
+  print_endline (ft_crypt "abcde" func_list);
+  print_endline("CASE 2");
+  print_endline(rot42 "XfRdE23");
+  print_endline(caesar 42 "XfRdE23");
+  print_endline(xor 21 "XfRdE23");
+  print_endline(rot42 (caesar 42 (xor 21 "XfRdE23")));
+  let func_list = [xor 21;caesar 42;rot42] in
+  print_endline (ft_crypt "XfRdE23" func_list)
 
 let _ = main()
--- day03/ex02/uncipher.ml
+++ day03/ex02/uncipher.ml
@@ -20,24 +20,38 @@ let uncaesar x rot_str =
     if up_case c || low_case c
     then begin
       if low_case c
-      then char_of_int (int_of_char 'a' + ((int_of_char c - int_of_char 'a' - x) mod 26))
-      else char_of_int (int_of_char 'A' + ((int_of_char c - int_of_char 'A' - x) mod 26))
+      then char_of_int (int_of_char 'z' + ((int_of_char c - int_of_char 'z' - x) mod 26))
+      else char_of_int (int_of_char 'Z' + ((int_of_char c - int_of_char 'Z' - x) mod 26))
       end
     else c
   in String.map shift_fun rot_str
 
 let xor key src_str =
-  String.map (function x -> char_of_int ((int_of_char x )lxor key)) src_str
+  let shift_fun c =
+    if up_case c || low_case c
+    then char_of_int (int_of_char c lxor (key mod 26))
+    else c
+  in String.map shift_fun src_str
 
-let rec ft_uncrypt src_str func = 
+let rec ft_uncrypt (src_str: string) func = 
   match func with
   | h :: t -> ft_uncrypt (h src_str) t
-  | [] -> "End"
+  | [] -> src_str
 
 let main () =
-  print_endline(unrot42 "abcde");
-  print_endline(uncaesar 1 "abcde");
-  print_endline(xor 1 "abcde")
-
+  print_endline("CASE 1");
+  print_endline(unrot42 "abcde1");
+  print_endline(uncaesar 1 "abcde1");
+  print_endline(xor 1 "abcde1");
+  print_endline(unrot42 (uncaesar 1 (xor 1 "abcde")));
+  let func_list = [xor 1;uncaesar 1;unrot42] in
+  print_endline (ft_uncrypt "abcde" func_list);
+  print_endline("CASE 2");
+  print_endline(unrot42 "XfRdE23");
+  print_endline(uncaesar 42 "XfRdE23");
+  print_endline(xor 21 "XfRdE23");
+  print_endline(unrot42 (uncaesar 42 (xor 21 "XfRdE23")));
+  let func_list = [xor 21;uncaesar 42;unrot42] in
+  print_endline (ft_uncrypt "XfRdE23" func_list)
 
 let _ = main()
