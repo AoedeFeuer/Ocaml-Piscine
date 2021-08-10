378c5f8db32a0323a62e0fb565f851fe9e7f4d14
Qarl Weissnat <qweissna@am-d2.msk.21-school.ru>
2021-08-10 18:54:41 +0300

    test

--- day06/ex01/ex01.ml
+++ day06/ex01/ex01.ml
@@ -13,3 +13,4 @@ let () =
   let pairs = List.map (fun s -> (s, String.length s)) values in
   List.iter (fun (k,v) -> StringHashtbl.add ht k v) pairs;
   StringHashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht
+  
