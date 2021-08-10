da7d5ac1b57b1538c0f6841c231ae8c23f81e919
Qarl Weissnat <qweissna@mi-b3.msk.21-school.ru>
2021-08-10 16:21:51 +0300

    progress

--- day05/ex01/ft_ref.ml
+++ day05/ex01/ft_ref.ml
@@ -1,5 +1,17 @@
-type 'a ft_ref = { contents : 'a }
+type 'a ft_ref = { mutable contents : 'a }
 let get x = x.contents
 let return x = { contents = x}
 let set x y = x.contents <- y
 let bind x f:'b ft_ref = f (get x)
+
+let () =
+  let tmp = return "This is test" in
+  print_endline (get tmp);
+  ignore (set tmp "No test");
+  print_endline (get tmp);
+  let tmp_nb = return 0 in
+  print_int (get tmp_nb);
+  print_endline "";
+  ignore (set tmp_nb 42);
+  print_int (get tmp_nb);
+  print_endline ""
